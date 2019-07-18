d92dc285da9f70fdbae0719192e69755e00a8e6e
Shayan Mohsin <shaymotionfilms@gmail.com>
2019-07-18 21:42:43 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,10 +2,12 @@
 # out the current state.
 
 board = ["x","o","x","x"," "," "," "," "," "]
-display_board = ["  #{board[0]}  |,"]
-puts "   |   |   "
+display_board(board)
+puts " #{board[0]   | #{board[1]  | #{board[2]  "
 puts "__________"
-puts "   |   |   "
+puts " #{board[3]  | #{board[4]  |  #{board[5] "
 puts "__________"
-puts "   |   |   "
-puts display_board(board)
+puts " #{board[6]  | #{board[7]  |  #{board[8] "
+end
+
+display_board(board)
