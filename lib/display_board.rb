0d168540f4675df427b077cd5861a38764da6793
Shayan Mohsin <shaymotionfilms@gmail.com>
2019-07-18 21:12:49 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,10 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+
+board = [" "," "," "," "," "," "," "," "," "]
+display_board = ["  #{board[0]}  |,"]
+puts "__________"
+
+print "__________"
+
+puts display_board(board)
